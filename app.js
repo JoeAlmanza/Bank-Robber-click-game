8b052ee5c958176c0d8c102a2ef91962f9760a99
Joe Almanza <joealmanza2113@gmail.com>
2020-08-23 22:54:03 -0600

    style

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ function drawUpgrades(){
   let template = ""
   clickUpgrades.forEach(u => template += `<button class="bg-secondary" id="upgradeButton" onclick="buyClickUpgrade('${u.name}')"><b class="text-info">$${u.price} - ${u.name}</b> <br>x${u.quantity}  <br> <b class="text-success">Current Bonus:</b> $${u.quantity * u.multiplier}<br> every click</button>`)
 
-  automaticUpgrades.forEach(u => template += `<button class="bg-secondary "id="upgradeButton" onclick="buyAutomaticUpgrade('${u.name}')"><b class="text-info">$${u.price} - ${u.name}</b> <br> x${u.quantity}  <br> <b class="text-success">Current Bonus:</b> $${u.quantity * u.multiplier}<br> every 3 seconds</button>`)
+  automaticUpgrades.forEach(u => template += `<button class="bg-secondary" id="upgradeButton" onclick="buyAutomaticUpgrade('${u.name}')"><b class="text-info">$${u.price} - ${u.name}</b> <br> x${u.quantity}  <br> <b class="text-success">Current Bonus:</b> $${u.quantity * u.multiplier}<br> every 3 seconds</button>`)
   upgradeArea.innerHTML = template
 }
 
